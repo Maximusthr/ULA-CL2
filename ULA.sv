dd7e2e510fa333b2276e0f8ed763660fd27852ef
Arthur Maximus <arthurmcb321@gmail.com>
2025-03-06 19:40:52 -0300

    update: clean code

--- ULA.sv
+++ ULA.sv
@@ -16,82 +16,53 @@ always @(*) begin
 	else begin
 		case({mode, oper})
 			4'b0000:
-				begin
-					if (A + B >= 7'b1000000) 
-						overflow <= 1;
-					O <= A + B;
-				end
-			4'b0001: // subtracao
-				begin
-					{overflow, O} <= {1'b0, A} - {1'b0, B};
-				end
+                {overflow, O} <= {1'b0, A} + {1'b0, B};
+
+			4'b0001:
+				{overflow, O} <= {1'b0, A} - {1'b0, B};
+
 			4'b0010:
-				begin
-					if (A + ~(B) >= 7'b1000000)
-						overflow <= 1;
-					O <= A + ~(B);
-				end
-			4'b0011: // subtracao com inversor
-				begin
-					{overflow, O} <= {1'b0, A} + {1'b0, ~B} + 1; // ~B equivale a B + 1
-				end
-			4'b0100:
-				begin
-					if (A + 1 >= 7'b1000000)
-						overflow <= 1;
-					O <= A + 1;
-				end
-			4'b0101:
-				begin
-				if (A == 6'b000000)
-					overflow <= 1;
-				O <= A - 1;
-				end
-			4'b0110:
-				begin
-					if (B + 1 >= 7'b1000000)
-						overflow <= 1;
-					O <= B + 1;
-				end
-			4'b0111:
-				begin
-				if (B == 6'b000000)
-					overflow <= 1;
-				O <= B - 1;
-				end
-			4'b1000:
-				begin
-				O <= A & B;
-				end
-			4'b1001:
-				begin
-				O <= ~(A);
-				end
-			4'b1010:
-				begin
-				O <= ~(B);
-				end
-			4'b1011:
-				begin
-				O <= A | B;
-				end
-			4'b1100:
-				begin
-				O <= A ^ B;
-				end
-			4'b1101:
-				begin
-				O <= ~(A ^ B);
-				end
-			4'b1110:
-				begin
-				O <= A;
-				end
-			4'b1111:
-				begin
-				O <= B;
-				end
+                {overflow, O} <= {1'b0, A} + {1'b0, ~B};
+
+			4'b0011: 
+				{overflow, O} <= {1'b0, A} - {1'b0, ~B};
 			
+            4'b0100:
+                {overflow, O} <= {1'b0, A} + 1;
+				
+			4'b0101:
+                {overflow, O} <= {1'b0, A} - 1;
+				
+			4'b0110:
+                {overflow, O} <= {1'b0, B} + 1;
+
+			4'b0111:
+                {overflow, O} <= {1'b0, B} -1;
+
+			4'b1000:
+				O <= A & B;
+				
+			4'b1001:
+				O <= ~(A);
+            
+            4'b1010:
+			    O <= ~(B);
+				
+			4'b1011:
+				O <= A | B;
+				
+			4'b1100:
+				O <= A ^ B;
+			
+            4'b1101:
+				O <= ~(A & B);
+				
+			4'b1110:
+				O <= A;
+				
+			4'b1111:
+				O <= B;
+							
 			default:
 				begin
 				O = 6'b000000;
